ad861f5431cf2cfa3ed61fcd25d9a18269221f93
ji881111 <ji881111@naver.com>
2019-02-13 03:56:36 +0900

    todo2

--- todo.js
+++ todo.js
@@ -2,9 +2,22 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = "toDos";    
+const TODOS_LS = "toDos";   
 
-const toDos = []; 
+let toDos = []; 
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    
+    const cleanToDos = toDos.filter(function(toDo){
+        //console.log(toDo.id, parseInt(li.id))
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,6 +27,7 @@ function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     span.innerText = text;
