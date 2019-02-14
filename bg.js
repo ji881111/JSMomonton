43cab9399f26cb6b3300b6c7ae6df20af75a82c3
ji881111 <ji881111@naver.com>
2019-02-15 03:34:08 +0900

    css

--- bg.js
+++ bg.js
@@ -2,26 +2,31 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 4;
 
+/*
 function handleImgLoad(){
     console.log("finished loading");
 }
+*/
 
-function paintImage(imgNumber){
-    const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`;
-    image.classList.add("bgImage");
-    body.appendChild(image);
-    image.addEventListener("loadend", handleImgLoad);
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`;
+  //image.classList.add("bgImage");
+  body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3)), url(${
+    image.src
+  })`;
+  //body.appendChild(image);
+  //image.addEventListener("loadend", handleImgLoad);
 }
 
 function genRandom() {
-    const number = Math.floor(Math.random() * IMG_NUMBER);
-    return number;
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
 }
 
 function init() {
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
 }
 
 init();
